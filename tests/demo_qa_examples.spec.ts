7568c168b1fc893e022b24265258c5b44411931a
Paweł K <grylion02@gmail.com>
2024-05-25 14:12:33 +0200

    check Home Page and go to each main subpage

--- tests/demo_qa_examples.spec.ts
+++ tests/demo_qa_examples.spec.ts
@@ -1,15 +1,37 @@
 import {test, expect} from "@playwright/test"
 
-test.describe("Demo tests for DemoQA", () =>{
-    test("Go to page and verify elements on it", async ({page}) => {
-        await page.goto('https://demoqa.com/')
-        expect(await page.getByText('Elements')).toBeVisible()
-        expect(await page.getByText('Forms')).toBeVisible()
-        expect(await page.getByText('Alerts, Frame & Windows')).toBeVisible()
-        expect(await page.getByText('Widgets')).toBeVisible()
-        expect(await page.getByText('Interactions')).toBeVisible()
-        expect(await page.getByText('Book Store Application')).toBeVisible()
 
+
+test.beforeEach("Go to Home Page", async ({page}) =>{
+    await page.goto('https://demoqa.com/')
+})
+
+test.describe("Demo tests for DemoQA", () =>{
+    test("Verify main buttons on Home Page", async ({page}) => {
+        await expect(page.getByText('Elements')).toBeVisible()
+        await expect(page.getByText('Forms')).toBeVisible()
+        await expect(page.getByText('Alerts, Frame & Windows')).toBeVisible()
+        await expect(page.getByText('Widgets')).toBeVisible()
+        await expect(page.getByText('Interactions')).toBeVisible()
+        await expect(page.getByText('Book Store Application')).toBeVisible()
+        
+    })
+    test("Go to Elements page", async ({page}) => {
         await page.getByText('Elements').click()
+    })
+    test("Go to Forms page", async ({page}) => {
+        await page.getByText('Forms').click()
+    })
+    test("Go to Alerts, Frame & Windows page", async ({page}) => {
+        await page.getByText('Alerts, Frame & Windows').click()
+    })
+    test("Go to Widgets page", async ({page}) => {
+        await page.getByText('Widgets').click()
+    })
+    test("Go to Interactions page", async ({page}) => {
+        await page.getByText('Interactions').click()
+    })
+    test("Go to Book Store Application page", async ({page}) => {
+        await page.getByText('Book Store Application').click()
     })
 })
